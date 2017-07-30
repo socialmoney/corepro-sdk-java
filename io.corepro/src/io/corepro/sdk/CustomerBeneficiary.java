6e2ec988c8fdd69608dbc91c0d24bee43ed27808
Brock Weaver <brock.weaver@q2ebanking.com>
2017-07-30 08:11:02 -0500

    Added new customer beneficiary properties and methods.

--- io.corepro/src/io/corepro/sdk/CustomerBeneficiary.java
+++ io.corepro/src/io/corepro/sdk/CustomerBeneficiary.java
@@ -37,6 +37,17 @@ public class CustomerBeneficiary extends ModelBase {
 	private String taxId;
 	private String taxIdMasked;
 	private Boolean isActive;
+	private String addressLine1;
+    private String addressLine2;
+    private String addressLine3;
+    private String addressLine4;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String legalName1;
+    private String legalName2;
+    private String distributionDescription;
+    private Date lastModifiedDate;
 
 	public CustomerBeneficiary() {
 		super();
@@ -202,5 +213,92 @@ public class CustomerBeneficiary extends ModelBase {
 		this.isActive = isActive;
 	}
 
-	
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getAddressLine3() {
+        return addressLine3;
+    }
+
+    public void setAddressLine3(String addressLine3) {
+        this.addressLine3 = addressLine3;
+    }
+
+    public String getAddressLine4() {
+        return addressLine4;
+    }
+
+    public void setAddressLine4(String addressLine4) {
+        this.addressLine4 = addressLine4;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getLegalName1() {
+        return legalName1;
+    }
+
+    public void setLegalName1(String legalName1) {
+        this.legalName1 = legalName1;
+    }
+
+    public String getLegalName2() {
+        return legalName2;
+    }
+
+    public void setLegalName2(String legalName2) {
+        this.legalName2 = legalName2;
+    }
+
+    public String getDistributionDescription() {
+        return distributionDescription;
+    }
+
+    public void setDistributionDescription(String distributionDescription) {
+        this.distributionDescription = distributionDescription;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
 }
